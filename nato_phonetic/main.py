bba7777503cb8cbdba791bfd686c0a019b534a4d
Jenny Kao <jjjenny.kao@gmail.com>
2025-03-18 16:21:46 -0700

    add try_except logic

--- nato_phonetic/main.py
+++ nato_phonetic/main.py
@@ -6,11 +6,21 @@ nato_dic = {row.letter: row.code for (index, row) in df.iterrows()}
 #print(nato_dic)
 
 
-user_input = input("Enter your name: ").upper()
-output_list = [nato_dic[letter] for letter in user_input]
-print(output_list) 
 
-# option 2 
+
+def generate_phonetic():
+    user_input = input("Enter your word: ").upper()
+    try:
+        output_list = [nato_dic[letter] for letter in user_input]    
+    except KeyError:
+        print("Sorry, only letter in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+    
+generate_phonetic()
+
+# option 2 123
 #word_list = []
 #for character in user_input:
 #    value = nato_dic.get(character.capitalize())
